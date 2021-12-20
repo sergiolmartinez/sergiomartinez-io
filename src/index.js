0b63a34cd548fcf58af946c72e2d81ad06093eeb
sergiolmartinez <smart0586@gmail.com>
2021-12-20 15:26:39 -0800

    refactored folder structure

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App';
+
+
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route exact path='/' element={<App/>}/>
+    </Routes>
+  </Router>,
+  document.getElementById('root')
+);
