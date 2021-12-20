2b450b9b79801d6275dc35a7f680c30fcde0f66c
sergiolmartinez <smart0586@gmail.com>
2021-12-20 13:55:31 -0800

    setting up initial frontend, add smioenv for backend

---
+++ README.md
@@ -0,0 +1,10 @@
+**Activate the virtual environment**
+
+```
+./smio-env/Scripts/activate
+```
+
+**Install all packages**
+```
+pip3 install -r requirements.txt
+```
---
+++ frontend/src/components/App.js
@@ -0,0 +1,12 @@
+// import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to sergiomartinez.io</h1>
+      <div><h3>Site is under construction</h3></div>
+    </div>
+  );
+}
+
+export default App;
