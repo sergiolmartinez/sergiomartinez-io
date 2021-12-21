0090efa5df7eab4ae78b34c3fe0bde5e571e8e31
sergiolmartinez <smart0586@gmail.com>
2021-12-20 21:33:07 -0800

    added image to test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import '../App.css';
+import construction from './assets/under-construction.png'
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to sergiomartinez.io</h1>
       <div><h3>Site is under construction</h3></div>
+      <img src={construction} class="rounded mx-auto d-block" alt="under-construction-png" />
     </div>
   );
 }
